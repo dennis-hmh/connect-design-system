2bb3998dd3a3aed9c394fb13a9d5bd24967a9e0f
Michael Adamek <michael.adamek@hmhco.com>
2024-04-11 10:36:33 +0100

    Finished up component.

--- src/components/ButtonSplit.stories.tsx
+++ src/components/ButtonSplit.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ButtonSplit } from './ButtonSplit';
-import { DownArrow } from '../assets/images/arrow-down-white.svg';
 
 export default {
   component: ButtonSplit,
@@ -16,25 +15,7 @@ export default {
 };
 
 export const Primary = (args) => <ButtonSplit {...args} />;
-export const Secondary = (args) => <ButtonSplit {...args} />;
 
 Primary.args = {
-  childrenOne: 'Check',
-  childrenTwo: (
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-      <path
-        fill="#fff"
-        d="M4.707 7.293a1 1 0 0 0-1.414 1.414l6 6a1 1 0 0 0 1.414 0l6-6a1 1 0 0 0-1.414-1.414L10 12.586 4.707 7.293Z"
-      />
-    </svg>
-  ),
-  primary: true,
-  correct: false,
-  incorrect: false,
   disabled: false,
-};
-
-Secondary.args = {
-  ...Primary.args,
-  primary: false,
 };
--- src/components/ButtonSplit.tsx
+++ src/components/ButtonSplit.tsx
@@ -1,40 +1,70 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+const testData = [
+  { label: '', value: 'Item 1' },
+  { label: '', value: 'Item 2' },
+  { label: '', value: 'Item 3' },
+  { label: '', value: 'Item 4' },
+  { label: '', value: 'Item 5' },
+  { label: '', value: 'Item 6' },
+  { label: '', value: 'Item 7' },
+  { label: '', value: 'Item 8' },
+];
 
 export type ButtonSplitProps = {
-  childrenOne: React.ReactNode;
-  childrenTwo: React.ReactNode;
-  primary: boolean;
-  correct?: boolean;
-  incorrect?: boolean;
   disabled?: boolean;
 };
 
-export function ButtonSplit({
-  childrenOne,
-  childrenTwo,
-  primary,
-  correct,
-  incorrect,
-  disabled,
-}: ButtonSplitProps) {
-  const isPrimary = primary ? 'connect__button-primary' : 'connect__button-secondary';
-  const isCorrect = correct ? 'connect__button-correct' : '';
-  const isIncorrect = incorrect ? 'connect__button-incorrect' : '';
+export function ButtonSplit({ disabled }: ButtonSplitProps) {
+  const [name, setName] = useState('Split Button');
+  const [open, setOpen] = useState(false);
+
+  const drop = useRef<HTMLInputElement | null>(null);
+  function handleClick(e) {
+    if (!e.target.closest(`.${drop.current?.className}`) && open) {
+      setOpen(false);
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  });
 
   return (
-    <div className={`connect__button-split ${isPrimary} ${isCorrect} ${isIncorrect}`}>
+    <div className={`connect__button-split`} ref={drop}>
       <button type="button" className={``} disabled={disabled}>
-        {childrenOne}
+        {name}
       </button>
-      <button type="button" className={``} disabled={disabled}>
-        {childrenTwo}
+      <button
+        type="button"
+        className={``}
+        disabled={disabled}
+        onClick={() => setOpen((open) => !open)}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
+          <path
+            fill="#fff"
+            d="M4.707 7.293a1 1 0 0 0-1.414 1.414l6 6a1 1 0 0 0 1.414 0l6-6a1 1 0 0 0-1.414-1.414L10 12.586 4.707 7.293Z"
+          />
+        </svg>
       </button>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 1</li>
-        <li>Item 1</li>
-        <li>Item 1</li>
-      </ul>
+      {open && <ButtonSplitMenu data={testData} />}
     </div>
+  );
+}
+
+export type DropDownMenuProps = {
+  children: React.ReactNode;
+};
+
+export function ButtonSplitMenu({ data }) {
+  return (
+    <ul>
+      {data.map((item, index) => (
+        <li key={index}>{item.value}</li>
+      ))}
+    </ul>
   );
 }
