468ea1742f54b052909a311ed96fbea47f6fca19
“RockA” <arock91@icloud.com>
2024-04-19 15:33:45 +0100

    setup color file that can take colors from source, muted and saturated support

---
+++ src/utils/colors.ts
@@ -0,0 +1,20 @@
+export type ColorBase =
+  | 'yellow'
+  | 'golden'
+  | 'orange'
+  | 'red'
+  | 'cerise'
+  | 'purple'
+  | 'blue'
+  | 'aqua'
+  | 'turquoise'
+  | 'green'
+  | 'apple'
+  | 'gray'
+  | 'cool-gray'
+  | 'warm-gray';
+
+export type ColorShade = 5 | 10 | 15 | 20 | 25 | 30 | 35 | 40 | 45 | 50 | 55 | 60 | 65 | 70;
+
+export type Color = `${ColorBase}-${'c' | 'm' | 's'}${ColorShade}`
+| 'white';
