7be0508e85fbdd6279b69a273183d6c95ea33000
Michael Adamek <michael.adamek@hmhco.com>
2024-04-22 16:30:06 -0400

    Added submit and onclick events to button.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,23 +3,47 @@ import React from 'react';
 export type ButtonProps = {
   children: React.ReactNode;
   primary: boolean;
+  disabled?: boolean;
   correct?: boolean;
   incorrect?: boolean;
-  disabled?: boolean;
+  submit?: 'button' | 'submit';
+  buttonId?: string;
+  buttonRef?: React.Ref<HTMLButtonElement>; // Updated to use React.Ref for buttonRef
+  ariaLabel?: string;
+  eventName?: string;
+  clickHandler?: Function;
+  feedbackBtn?: 'feedback-button' | '';
 };
 
-export function Button({ children, primary, correct, incorrect, disabled }: ButtonProps) {
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  primary = true,
+  disabled = false,
+  correct,
+  incorrect,
+  submit = 'button',
+  buttonId,
+  buttonRef,
+  ariaLabel,
+  eventName,
+  clickHandler,
+  feedbackBtn = 'feedback-button',
+}) => {
   const isPrimary = primary ? 'connect__button-primary' : 'connect__button-secondary';
   const isCorrect = correct ? 'connect__button-correct' : '';
   const isIncorrect = incorrect ? 'connect__button-incorrect' : '';
 
   return (
     <button
-      type="button"
-      className={`connect__button ${isPrimary} ${isCorrect} ${isIncorrect}`}
+      ref={buttonRef}
+      id={buttonId ? buttonId : undefined}
+      aria-label={ariaLabel}
+      type={submit}
+      className={`connect__button ${feedbackBtn} ${isPrimary} ${isCorrect} ${isIncorrect}`}
+      onClick={clickHandler ? () => clickHandler(eventName) : undefined}
       disabled={disabled}
     >
       {children}
     </button>
   );
-}
+};
