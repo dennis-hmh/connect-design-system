9738b10b889b2ef4ef046334ba3aea445ac0f9a4
Michael Adamek <michael.adamek@hmhco.com>
2024-04-23 15:33:42 -0400

    Updated input to be controlled component. Update focus-visible styles.

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
-//import { useState } from 'react';
+import { useState } from 'react';
 
 export type InputTextProps = {
   correct: boolean;
   incorrect: boolean;
-  value: string | number;
   number?: boolean;
   disabled?: boolean;
 };
 
-export function InputText({ correct, incorrect, value, number, disabled }: InputTextProps) {
+export function InputText({ correct, incorrect, number, disabled }: InputTextProps) {
   const isCorrect = correct ? 'connect__input-correct' : '';
   const isIncorrect = incorrect ? 'connect__input-incorrect' : '';
   const isNumber = number ? 'number' : 'text';
 
-  //const [text, setText] = useState('');
+  const [text, setText] = useState('');
 
   return (
     <label className="status">
       <input
         type={isNumber}
         className={`connect__input ${isCorrect} ${isIncorrect}`}
-        defaultValue={value}
         disabled={disabled}
-        //value={text}
-        //onChange={(e) => setText(e.target.value)}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
     </label>
   );
