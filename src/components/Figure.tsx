2e3f16b09daf253aa08b5fd895b66ea7ae4aaff3
Michael Adamek <michael.adamek@hmhco.com>
2024-05-06 15:21:06 -0400

    Update to more generic name to be used outside of the Card component.

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export type CardFigureProps = {
+export type FigureProps = {
   altText: string;
   imageSrc: string;
   imageCaption?: string;
   dataTestId?: string;
 };
 
-export function CardFigure({ altText, imageSrc, imageCaption, dataTestId }: CardFigureProps) {
+export function Figure({ altText, imageSrc, imageCaption, dataTestId }: FigureProps) {
   return (
     <figure data-testid={dataTestId}>
       <img alt={altText} src={imageSrc} />
