fd6baa0d5ca7c65d32dc17c70e85106fb6118d22
Michael Adamek <michael.adamek@hmhco.com>
2024-08-12 16:44:31 -0400

    Migrate to Storybook 8.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,16 +6,18 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+
   stories: [
     '../src/**/**/*.stories.@(js|jsx|ts|tsx)',
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+
   core: {
     builder: '@storybook/builder-vite',
   },
-  features: {
-    legacyMdx1: true,
-  },
+
+  features: {},
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -25,10 +27,11 @@ const config: StorybookConfig = {
     'storybook-addon-pseudo-states',
     'storybook-addon-themes',
     'storybook-addon-react-docgen',
+    '@chromatic-com/storybook'
   ],
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
   managerHead: (head) => `
     ${head}
     <style>
@@ -37,12 +40,17 @@ const config: StorybookConfig = {
       button[id="reveal"] {display: none; }
     </style>
   `,
+
   async viteFinal(config) {
     return {
       ...config,
       plugins: await withoutVitePlugins(config.plugins, ['vite:lib-inject-css']),
     };
   },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -27,7 +27,6 @@ const preview: Preview = {
         },
       ],
     },
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
