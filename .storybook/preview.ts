015c03b31c126db6162ccbd42bfb611ec3ef2f61
Michael Adamek <michael.adamek@hmhco.com>
2024-08-15 11:59:25 -0400

    Removed commented out code.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -12,27 +12,6 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
-    /* themes: {
-      default: 'Grade 4-5',
-      list: [
-        {
-          name: 'Grade 2-3',
-          class: 'connect__g23',
-        },
-        {
-          name: 'Grade 4-5',
-          class: 'connect__g45',
-        },
-        {
-          name: 'Grade 6-8',
-          class: 'connect__g68',
-        },
-        {
-          name: 'Grade 9-12',
-          class: 'connect__g912',
-        },
-      ],
-    }, */
   },
 };
 
