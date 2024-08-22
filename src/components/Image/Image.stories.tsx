3604fc0554f42952d34ed3cadaf9eb8a53866e56
Michael Adamek <michael.adamek@hmhco.com>
2024-08-22 11:29:45 -0400

    Add the Image component.

---
+++ src/components/Image/Image.stories.tsx
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react';
+import { Meta, StoryObj, StoryFn } from '@storybook/react';
+import { Image, ImageProps } from './Image';
+import { ConnectTheme } from '../ConnectTheme';
+import { GradeBand } from '../../enum/gradeband';
+
+const meta: Meta<typeof Image> = {
+  title: 'Image',
+  component: Image,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Image>;
+
+const Template: StoryFn<ImageProps> = (args) => {
+  const themeWrapperRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <ConnectTheme gradeBand={args.gradeBand} themeWrapperRef={themeWrapperRef}>
+      <div ref={themeWrapperRef}>
+        <Image {...args} />
+      </div>
+    </ConnectTheme>
+  );
+};
+
+export const Default: Story = Template.bind({});
+Default.args = {
+  imageSrc: '',
+  altText: 'This is Alt Text',
+  gradeBand: GradeBand.G4_5,
+};
