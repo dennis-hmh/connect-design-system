b31a9affd3a9d2dc7fe4a540451f58a18bc0a85e
Michael Adamek <michael.adamek@hmhco.com>
2024-10-28 12:50:51 -0400

    Updated to ESLint v9^

---
+++ eslint.config.mjs
@@ -0,0 +1,101 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import react from 'eslint-plugin-react';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-plugin-prettier';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      '**/dist',
+      '**/storybook-static',
+      '**/.eslintrc.js',
+      'src/components/**/*.stories.tsx',
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ),
+  ),
+  {
+    plugins: {
+      react: fixupPluginRules(react),
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      'react-hooks': fixupPluginRules(reactHooks),
+      prettier,
+      'react-refresh': reactRefresh,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        project: './tsconfig.json',
+      },
+    },
+
+    settings: {
+      react: {
+        version: '18.2',
+      },
+
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+
+    rules: {
+      camelcase: 'off',
+      'no-duplicate-imports': 'error',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-console': 'error',
+      'no-alert': 'error',
+      'react-hooks/exhaustive-deps': 'off',
+      'react/prop-types': 0,
+      'react/display-name': 0,
+      '@typescript-eslint/no-empty-function': 'off',
+      'react/no-unknown-property': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+    },
+  },
+];
