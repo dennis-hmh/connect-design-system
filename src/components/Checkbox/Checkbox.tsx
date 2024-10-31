11160562e4853a02b876f4d8a15fdde322212f01
Michael Adamek <michael.adamek@hmhco.com>
2024-10-31 12:00:04 -0400

    Add a Checkbox variant of Input Box with only specific props.

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { GradeBand } from 'src/enum/gradeband';
+
+export type CheckboxProps = {
+  id: string;
+  name: string;
+  children: React.ReactNode;
+  checked?: boolean;
+  disabled?: boolean;
+  dataTestId?: string;
+  gradeBand?: GradeBand;
+};
+
+export function Checkbox({ id, name, children, checked, disabled, dataTestId }: CheckboxProps) {
+  const checkRef = useRef<HTMLInputElement>(null);
+  const [isChecked, setIsChecked] = useState(checked || false);
+  const handleChange = () => {
+    setIsChecked(checkRef.current?.checked ?? false);
+  };
+
+  useEffect(() => {
+    setIsChecked(checked || false);
+  }, [checked]);
+
+  return (
+    <div className="connect__label-wrapper">
+      <input
+        ref={checkRef}
+        type="checkbox"
+        id={id}
+        className={`connect__input }`}
+        name={name}
+        checked={isChecked}
+        onChange={handleChange}
+        disabled={disabled}
+        data-testid={dataTestId}
+      />
+      <label htmlFor={id} className={`connect__checkbox-label `}>
+        {children}
+      </label>
+    </div>
+  );
+}
