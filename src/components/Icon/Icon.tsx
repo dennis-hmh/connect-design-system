557344f1362299df1bfdf3b092fcad5961d2281f
Michael Adamek <michael.adamek@hmhco.com>
2024-11-25 16:20:15 -0500

    Component tune-up.

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Color } from '../../utils/colors';
 import { IconId } from '../../utils/icon-list';
 import { GradeBand } from 'src/enum/gradeband';
-// import sprite from '/dist/svg/sprite.svg';
-// import sprite from '/public/svg/sprite.svg';
-import spriteSheet from '../../assets/icons/sprite.svg'
+import spriteSheet from '../../assets/icons/sprite.svg';
 
 export type IconProps = {
   id: IconId;
@@ -28,7 +26,6 @@ export const Icon: React.FC<IconProps> = ({
   className,
   dataTestId,
 }) => {
-
   const [symbolContent, setSymbolContent] = useState('');
 
   useEffect(() => {
@@ -38,7 +35,6 @@ export const Icon: React.FC<IconProps> = ({
         if (!response.ok) throw new Error(`Failed to load SVG: ${response.statusText}`);
         const svgContent = await response.text();
 
-        // Parse the SVG content and extract the desired symbol
         const parser = new DOMParser();
         const svgDocument = parser.parseFromString(svgContent, 'image/svg+xml');
         const symbol = svgDocument.getElementById(id);
@@ -56,37 +52,25 @@ export const Icon: React.FC<IconProps> = ({
     loadIcon();
   }, [spriteSheet, id]);
 
-
-    
   const fillColorVariable = fill ? `--connect__${fill}` : '';
   const strokeColorVariable = fill ? `--connect__${stroke}` : '';
 
-  // const spriteUrl =
-  //   import.meta.env.PROD === true && import.meta.env.VITE_ENV !== 'chromatic'
-  //     ? '/node_modules/@connect/connect-design-system/dist/svg/sprite.svg'
-  //     : '/svg/sprite.svg';
-
-      return (
-        <svg
-          className={`connect__icon connect__icon-${size} ${className}`}
-          style={
-            {
-              '--connect__icon-fill-color': `var(${fillColorVariable})`,
-              '--connect__icon-stroke-color': `var(${strokeColorVariable})`,
-              '--connect__icon-opacity': `${opacity}`,
-            } as React.CSSProperties
-          }
-          aria-hidden="true"
-          focusable={focusable}
-          data-testid={dataTestId}
-        >
-          {symbolContent && (
-            <g
-              dangerouslySetInnerHTML={{ __html: symbolContent }}
-            />
-          )}
-          <use href={`#${id}`} />
-        </svg>
-      );
-    };
-    
+  return (
+    <svg
+      className={`connect__icon connect__icon-${size} ${className ? className : ''}`}
+      style={
+        {
+          '--connect__icon-fill-color': `var(${fillColorVariable})`,
+          '--connect__icon-stroke-color': `var(${strokeColorVariable})`,
+          '--connect__icon-opacity': `${opacity}`,
+        } as React.CSSProperties
+      }
+      aria-hidden="true"
+      focusable={focusable}
+      data-testid={dataTestId}
+    >
+      {symbolContent && <g dangerouslySetInnerHTML={{ __html: symbolContent }} />}
+      <use href={`#${id}`} />
+    </svg>
+  );
+};
