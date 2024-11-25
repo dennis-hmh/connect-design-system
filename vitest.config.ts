dde84461ff4d1de0d4a2d12ab4c7ec1267d60cfe
Michael Adamek <michael.adamek@hmhco.com>
2024-11-25 11:53:41 -0500

    Updated and simplfied vistest.

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,25 +12,24 @@ export default mergeConfig(
         storybookScript: 'pnpm storybook --ci',
       }),
     ],
-    // Test now handled in vitest.workspace.ts
-    // test: {
-    //   include: ['src/components/**/*.stories.@(ts|tsx)'],
-    //   exclude: [
-    //     'src/components/DragDrop/DragDrop.stories.@(jsx|tsx)',
-    //     'src/components/FlipCards/FrontCard.stories.@(jsx|tsx)',
-    //     'src/components/Reveal/Reveal.stories.@(jsx|tsx)',
-    //   ],
-    //   browser: {
-    //     enabled: true,
-    //     name: 'chromium',
-    //   },
-    //   globals: true,
-    //   environment: 'jsdom',
-    //   coverage: {
-    //     enabled: false,
-    //   },
-    //   isolate: false,
-    //   setupFiles: ['./.storybook/vitest.setup.ts'],
-    // },
+    test: {
+      include: ['src/components/**/*.stories.@(ts|tsx)'],
+      exclude: [
+        'src/components/DragDrop/DragDrop.stories.@(jsx|tsx)',
+        'src/components/FlipCards/FrontCard.stories.@(jsx|tsx)',
+        'src/components/Reveal/Reveal.stories.@(jsx|tsx)',
+      ],
+      browser: {
+        enabled: true,
+        name: 'chromium',
+      },
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        enabled: false,
+      },
+      isolate: false,
+      setupFiles: ['./.storybook/vitest.setup.ts'],
+    },
   }),
 );
