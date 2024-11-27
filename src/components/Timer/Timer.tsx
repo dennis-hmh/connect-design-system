22ba6252049a17977cc4ea2ff71e798d0a289de6
Michael Adamek <michael.adamek@hmhco.com>
2024-11-27 09:20:13 -0500

    Updated var to let.

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -22,7 +22,7 @@ export type TimerProps = {
   progressBar?: boolean;
   dataTestId?: string;
   gradeBand?: GradeBand;
-  parentState?: number,
+  parentState?: number;
 };
 
 export enum timerStates {
@@ -30,7 +30,7 @@ export enum timerStates {
   counting_down,
   finished,
   paused,
-};
+}
 
 interface CustomCSSProperties extends React.CSSProperties {
   '--connect__timer-fs'?: string;
@@ -59,74 +59,66 @@ export const Timer: React.FC<TimerProps> = ({
   const initialStateValue = parentState == undefined ? 1 : parentState;
   const [componentState, setComponentState] = useState(initialStateValue);
 
-  const isWaiting =       componentState == timerStates.waiting_to_start;
-  const isCountingDown =  componentState == timerStates.counting_down;
-  const isFinished =      componentState == timerStates.finished;
-  const isPaused =        componentState == timerStates.paused;
+  const isWaiting = componentState == timerStates.waiting_to_start;
+  const isCountingDown = componentState == timerStates.counting_down;
+  const isFinished = componentState == timerStates.finished;
+  const isPaused = componentState == timerStates.paused;
 
   /* Watch for changes in this Timer component's state */
   useEffect(() => {
     //When entering the waiting state, reset the countdown's progress
-    if (isWaiting){
+    if (isWaiting) {
       setRemainingTime(time);
     }
 
     /* Call the onTimeUp function, if one was provided 
       (It's important to do this here via a useEffect rather than in the countdown's setInterval, so the onTimeUp function
       can potentially change the state of the parent component, without causing a bad setState() call) */
-    if (isFinished){
+    if (isFinished) {
       setRemainingTime(0);
 
-      if (onTimeUp && onTimeUp instanceof Function)
-        onTimeUp();
+      if (onTimeUp && onTimeUp instanceof Function) onTimeUp();
     }
   }, [componentState]);
 
   /* If the parent's state value was provided, make any changes in its value update the state of this Timer component */
   useEffect(() => {
-    if (parentState == undefined)
-      return;
+    if (parentState == undefined) return;
 
     //Update this component's state based on the parent's state (only allowing values that actually represent something)
     const clampedValue = Math.max(Math.min(parentState, 3), 0);
     setComponentState(clampedValue);
+  }, [parentState, componentState]);
 
-  }, [parentState, componentState])
-
-  
   //Countdown behaviour
   const updateInterval = 1000;
   useEffect(() => {
     //If the timer isn't counting down or paused, reset anything to do with calculating the remaining time
-    if (isWaiting || isFinished){
-      countdownTotalTimeSpentPaused.current = 0; 
-      countdownStartedAt.current = null; 
+    if (isWaiting || isFinished) {
+      countdownTotalTimeSpentPaused.current = 0;
+      countdownStartedAt.current = null;
       countdownPausedAt.current = null;
     }
 
     //Handle pausing
-    if (isPaused){
-      if (!countdownPausedAt.current)
-        countdownPausedAt.current = Date.now();
-    }
-    else if (countdownPausedAt.current != null){
+    if (isPaused) {
+      if (!countdownPausedAt.current) countdownPausedAt.current = Date.now();
+    } else if (countdownPausedAt.current != null) {
       countdownTotalTimeSpentPaused.current += Date.now() - countdownPausedAt.current;
       countdownPausedAt.current = null;
     }
 
     //Handle counting down
-    if (!isCountingDown)
-      return;
+    if (!isCountingDown) return;
 
-    if (countdownStartedAt.current == null)
-      countdownStartedAt.current = Date.now();
+    if (countdownStartedAt.current == null) countdownStartedAt.current = Date.now();
 
     intervalRef.current = setInterval(() => {
       setRemainingTime((prevTime) => {
-        if (countdownStartedAt.current == null)
-          return time;
+        if (countdownStartedAt.current == null) return time;
 
-        let newTime = time - (Date.now() - countdownStartedAt.current - countdownTotalTimeSpentPaused.current);
+        let newTime =
+          time - (Date.now() - countdownStartedAt.current - countdownTotalTimeSpentPaused.current);
 
         if (newTime <= 0 || prevTime <= 0) {
           clearInterval(intervalRef.current!);
@@ -151,12 +143,12 @@ export const Timer: React.FC<TimerProps> = ({
   }, [time]);
 
   //Quantise the progress wrt. the update interval
-  var nearestMultiple = Math.max(0, Math.round(remainingTime/updateInterval)*updateInterval);
-  const roundingCorrection = Math.ceil(Math.round(nearestMultiple - (updateInterval/3))/1000);
-  
+  let nearestMultiple = Math.max(0, Math.round(remainingTime / updateInterval) * updateInterval);
+  const roundingCorrection = Math.ceil(Math.round(nearestMultiple - updateInterval / 3) / 1000);
+
   //Calculate h/m/s for the time string
-  const hours = Math.floor(roundingCorrection/360);
-  const minutes = Math.floor(roundingCorrection/60) % 60;
+  const hours = Math.floor(roundingCorrection / 360);
+  const minutes = Math.floor(roundingCorrection / 60) % 60;
   const seconds = roundingCorrection % 60;
 
   const formattedTime = hours
