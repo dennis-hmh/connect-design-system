b1fc98d2c6f81fecfea591efd2250b65caece8ea
“RockA” <Andrew.Rock@hmhco.com>
2024-11-27 16:35:53 +0000

    take the component that is conflicting

--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -8,6 +8,8 @@ export type InputTextProps = {
   answerShown?: boolean;
   number?: boolean;
   disabled?: boolean;
+  characterCount?: boolean;
+  placeholderText?: string | undefined;
   defaultText?: string | number;
   dataTestId?: string;
   gradeBand?: GradeBand;
@@ -19,15 +21,19 @@ export function InputText({
   answerShown,
   number,
   disabled,
+  characterCount,
+  placeholderText,
+  characterLimit,
   defaultText,
   dataTestId,
 }: InputTextProps) {
-  const [text, setText] = useState(defaultText);
-  const [isSelected, setIsSelected] = useState(false);
-
-  const inputStates = `${correct ? 'connect__feedback-correct' : ''} ${incorrect ? 'connect__feedback-incorrect' : ''} ${answerShown ? 'connect__feedback-shown' : ''} ${isSelected ? 'connect__selected' : ''}`;
+  const inputStates = `${correct ? 'connect__feedback-correct' : ''} ${incorrect ? 'connect__feedback-incorrect' : ''} ${answerShown ? 'connect__feedback-shown' : ''} ${isSelected ? 'connect__selected' : ''} ${characterCount ? 'connect__input-character-count' : ''}`;
 
   const isNumber = number ? 'number' : 'text';
+
+  const [text, setText] = useState(defaultText);
+  const [charCount, setCharCount] = useState(defaultText?.length || 0);
+  const [isSelected, setIsSelected] = useState(false);
 
   let inputAriaLabel = 'Input field';
   if (correct) {
@@ -40,6 +46,12 @@ export function InputText({
 
   const shouldBeDisabled = correct || incorrect || answerShown || disabled;
 
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    setText(newText);
+    setCharCount(newText.length);
+  };
+
   return (
     <label className="connect__icon-wrapper">
       <input
@@ -47,12 +59,25 @@ export function InputText({
         className={`connect__input ${inputStates} ${disabled ? 'connect__disabled' : ''}`}
         disabled={shouldBeDisabled}
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        placeholder={placeholderText ? placeholderText : ''}
+        onChange={handleTextChange}
         onMouseDown={() => setIsSelected(true)}
         onBlur={() => setIsSelected(false)}
         aria-label={inputAriaLabel}
         data-testid={dataTestId}
       />
+      {characterCount && (
+        <div
+          className={`connect__character-counter ${
+            characterLimit && charCount >= characterLimit
+              ? 'connect__character-counter-limit-reached'
+              : ''
+          }`}
+        >
+          <em>{charCount}</em>
+          {characterLimit ? ` / ${characterLimit}` : ''}
+        </div>
+      )}
     </label>
   );
 }
