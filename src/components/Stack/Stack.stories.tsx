20d9a7ca4dbee3ffeb3bae8d072f913fe78c24f1
“RockA” <Andrew.Rock@hmhco.com>
2024-12-02 14:33:26 +0000

    add the stack

--- src/components/Stack/Stack.stories.tsx
+++ src/components/Stack/Stack.stories.tsx
@@ -1,8 +1,14 @@
 import React, { useRef } from 'react';
 import { Meta, StoryObj, StoryFn } from '@storybook/react';
-import { Stack, StackProps } from './Stack';
+import { Stack, StackProps } from '../Stack/Stack';
 import { ConnectTheme } from '../ConnectTheme';
 import { GradeBand } from '../../enum/gradeband';
+
+import { Paper } from '../Paper/Paper';
+import { Figure } from '../Figure/Figure';
+import { Image } from '../Image/Image'; // Import Image
+import { Button } from '../Button/Button';
+import { Icon } from '../Icon/Icon';
 
 const meta: Meta<typeof Stack> = {
   title: 'Layout/Stack',
@@ -19,60 +25,25 @@ const Template: StoryFn<StackProps> = (args) => {
   return (
     <ConnectTheme gradeBand={GradeBand.G4_5} themeWrapperRef={themeWrapperRef}>
       <div ref={themeWrapperRef}>
-        <Stack {...args}>
-          <Stack sm={{ direction: 'row', spacing: 'sm' }}>
-            <div
-              style={{
-                background: 'var(--connect__correct-light)',
-                padding: 'var(--connect__spacer-md)',
-              }}
+        <Paper elevation={2} roundedCorner={true}>
+          <Stack {...args}>
+            <Figure caption="This is a caption">
+              <Image imageSrc={'../../images/zelda.jpg'} altText={'This is Alt Text'} />
+            </Figure>
+
+            <Stack
+              xs={{ direction: 'column', spacing: 'sm', paddingX: 'sm', paddingY: 'md' }}
+              lg={{ direction: 'row', justifyContent: 'space-between' }}
             >
-              1 row
-            </div>
-            <div
-              style={{
-                background: 'var(--connect__correct-mid)',
-                padding: 'var(--connect__spacer-md)',
-              }}
-            >
-              2 row
-            </div>
-            <div
-              style={{
-                background: 'var(--connect__correct-light)',
-                padding: 'var(--connect__spacer-md)',
-              }}
-            >
-              3 row
-            </div>
+              <Button primary={true} disabled={true}>
+                Cancel
+              </Button>
+              <Button primary={true}>
+                <Icon id="check" size="sm" /> Save
+              </Button>
+            </Stack>
           </Stack>
-          <Stack sm={{ direction: 'column', spacing: 'sm' }}>
-            <div
-              style={{
-                background: 'var(--connect__correct-light)',
-                padding: 'var(--connect__spacer-md)',
-              }}
-            >
-              1 column
-            </div>
-            <div
-              style={{
-                background: 'var(--connect__correct-mid)',
-                padding: 'var(--connect__spacer-md)',
-              }}
-            >
-              2 column
-            </div>
-            <div
-              style={{
-                background: 'var(--connect__correct-light)',
-                padding: 'var(--connect__spacer-md)',
-              }}
-            >
-              3 column
-            </div>
-          </Stack>
-        </Stack>
+        </Paper>
       </div>
     </ConnectTheme>
   );
@@ -81,9 +52,7 @@ const Template: StoryFn<StackProps> = (args) => {
 export const Default: Story = Template.bind({});
 Default.args = {
   sm: { direction: 'column', spacing: 'sm' },
-  md: { direction: 'row', spacing: 'md', alignItems: 'center', justifyContent: 'space-between' },
-  lg: { spacing: 'lg', paddingX: 'lg', paddingY: 'lg' },
-  xl: { spacing: 'xl', paddingX: 'xl', paddingY: 'xl' },
+  md: { direction: 'row', alignItems: 'center', justifyContent: 'space-between' },
   className: '',
   dataTestId: '',
 };
