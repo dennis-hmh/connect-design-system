a4372b64e865693ea4b1f638fbfd42a215688056
Michael Adamek <michael.adamek@hmhco.com>
2025-01-22 15:54:17 -0500

    Added Dropdown component.

---
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { Meta, StoryObj, StoryFn } from '@storybook/react';
+import { Dropdown, DropdownProps } from './Dropdown';
+import { DropdownMenu, DropdownMenuProps } from './Dropdown';
+import { ConnectTheme } from '../ConnectTheme';
+import { GradeBand } from '../../enum/gradeband';
+
+const meta: Meta<typeof Dropdown> = {
+  title: 'Misc/Dropdown',
+  component: Dropdown,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Dropdown>;
+
+const Template: StoryFn<DropdownProps> = (args) => {
+  const themeWrapperRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <ConnectTheme gradeBand={args.gradeBand} themeWrapperRef={themeWrapperRef}>
+      <div ref={themeWrapperRef}>
+        <Dropdown {...args} />
+      </div>
+    </ConnectTheme>
+  );
+};
+
+export const Default: Story = Template.bind({});
+Default.args = {
+  children: 'Select an option',
+  data: [
+    { label: 'Option 1', className: null, ariaSelected: false, value: 'option1' },
+    { label: 'Option 2', className: null, ariaSelected: false, value: 'option2' },
+    { label: 'Option 3', className: 'is-focused', ariaSelected: true, value: 'option3' },
+    { label: 'Option 4', className: null, ariaSelected: false, value: 'option4' },
+    { label: 'Option 5', className: null, ariaSelected: false, value: 'option5' },
+    { label: 'Option 6', className: null, ariaSelected: false, value: 'option6' },
+    { label: 'Option 7', className: 'connect__disabled', ariaSelected: false, value: 'option7' },
+  ],
+  disabled: false,
+  gradeBand: GradeBand.G4_5,
+};
+
+export const Hint: Story = Template.bind({});
+Hint.args = {
+  ...Default.args,
+  children: 'Select a name',
+  data: [
+    { label: 'Alice', value: 'alice' },
+    { label: 'Bob', value: 'bob' },
+    { label: 'Christopher', value: 'christopher' },
+    { label: 'David', value: 'david' },
+    { label: 'Eve', value: 'eve' },
+    { label: 'Fitzgerald', value: 'fitzgerald' },
+    { label: 'Grace', value: 'grace' },
+  ],
+  hint: 'Choose a name',
+};
---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { Hint } from '../Hint/Hint';
+import { GradeBand } from 'src/enum/gradeband';
+
+export type DropdownProps = {
+  children: React.ReactNode;
+  data: { label: string; value: React.ReactNode }[];
+  label?: string;
+  hint?: string;
+  correct?: boolean;
+  incorrect?: boolean;
+  answerShown?: boolean;
+  disabled?: boolean;
+  dataTestId?: string;
+  gradeBand?: GradeBand;
+};
+
+export const Dropdown: React.FC<DropdownProps> = ({
+  children,
+  data,
+  label,
+  hint,
+  correct,
+  incorrect,
+  answerShown,
+  disabled = false,
+  dataTestId,
+}) => {
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+
+  const feedbackStates = [
+    correct && 'connect__feedback-correct',
+    incorrect && 'connect__feedback-incorrect',
+    answerShown && 'connect__feedback-shown',
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  const handleClick = () => {
+    if (!disabled) {
+      setOpen(!open);
+    }
+  };
+
+  const handleItemClick = (label: string) => {
+    setSelectedValue(label);
+    setOpen(false);
+  };
+
+  return (
+    <div
+      id="connect__dropdown-button"
+      className={`connect__dropdown ${feedbackStates} ${open ? 'connect__dropdown-open' : ''}`}
+      role="button"
+      aria-haspopup="listbox"
+      aria-labelledby="connect__dropdown-label"
+      aria-expanded={open}
+      onClick={handleClick}
+      disabled={disabled}
+      tabIndex={0}
+      data-testid={dataTestId}
+    >
+      <span id="connect__dropdown-label" className="connect__dropdown-label" hidden>
+        {label || children}
+      </span>
+      <span id="connect__selected-text-id" className="connect__selected-text">
+        {selectedValue || children}
+      </span>
+      {hint && <Hint>{hint}</Hint>}
+      {open && (
+        <div className="connect__dropdown-menu" aria-labelledby="connect__dropdown-label">
+          <DropdownMenu data={data} onItemClick={handleItemClick} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export type DropdownMenuProps = {
+  data: { label: string; className: string | null; ariaSelected: boolean; value: string }[];
+  onItemClick: (label: string) => void;
+};
+
+export const DropdownMenu: React.FC<DropdownMenuProps> = ({ data, onItemClick }) => {
+  return (
+    <ul className="connect__dropdown-list-menu" role="listbox">
+      {data.map((item, index) => (
+        <li
+          key={`connect__menu-item-${index}`}
+          className={`connect__dropdown-item ${item.className}`}
+          role="option"
+          aria-selected={item.ariaSelected || false}
+          onClick={() => onItemClick(item.label)}
+        >
+          {item.label}
+        </li>
+      ))}
+    </ul>
+  );
+};
