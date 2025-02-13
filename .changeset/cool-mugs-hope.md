fcd8bd166a58513c262c07dfa7cda9cf0f3c2cb9
Comeau, Samuel <Samuel.Comeau@hmhco.com>
2025-02-13 13:08:48 -0500

    Move Font files to the Lumina CDN to reduce package size (#173)
    * Move Font files to the Lumina CDN to reduce package size
    
    * fixup! Move Font files to the Lumina CDN to reduce package size
    
    * fixup! Move Font files to the Lumina CDN to reduce package size
    
    ---------
    
    Co-authored-by: Samuel Comeau <24446727+sam-com@users.noreply.github.com>

---
+++ .changeset/cool-mugs-hope.md
@@ -0,0 +1,5 @@
+---
+'@connect/connect-design-system': patch
+---
+
+Move Font files to the Lumina CDN to reduce package size
--- src/components/RiveEngine/index.ts
+++ src/components/RiveEngine/index.ts
@@ -1,2 +1,3 @@
+export * from "@rive-app/react-canvas";
 export * from "./RiveEngine";
 export * from "./useReactStateMachineInput";
