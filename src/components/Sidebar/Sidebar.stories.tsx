e13b3402e1917cff0e9113d4422ce01a99236888
Michael Adamek <michael.adamek@hmhco.com>
2025-02-24 14:03:31 -0500

    Updated Pattern container name.

--- src/components/Sidebar/Sidebar.stories.tsx
+++ src/components/Sidebar/Sidebar.stories.tsx
@@ -10,7 +10,7 @@ import { GradeBand } from '../../enum/gradeband';
 import { ButtonMenuProvider } from '../../context/ButtonMenuContext';
 
 const meta: Meta<typeof Sidebar> = {
-  title: 'Patter/Sidebar',
+  title: 'Pattern/Sidebar',
   component: Sidebar,
   tags: ['autodocs'],
   parameters: {
